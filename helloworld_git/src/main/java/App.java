80d339756c77ee959c472e3cdf4c5ce30c85a051
it137678620 <137678620@qq.com>
2019-07-26 18:03:15 +0800

    测试1

---
+++ helloworld_git/src/main/java/App.java
@@ -0,0 +1,10 @@
+/**
+ * @author xw
+ * @version v 1.0
+ * @date 2019/7/26 17:59
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello  我来了 ");
+    }
+}
